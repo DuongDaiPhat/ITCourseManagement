18ef4e3676426b6d31e6e0651ca0988b1bbed547
KimDungwdf <6451071009@st.utc2.edu.vn>
2025-03-24 02:42:24 +0700

    Create classes
    Tạo các class Lecture, LectureProgress, Categories, CourseCategories, MyLearning,  Certificate, UserCertificate, MyWishList.

---
+++ src/entity/Certificate.java
@@ -0,0 +1,40 @@
+package entity;
+
+public class Certificate {
+    private int certificateId;     
+    private String certificateName; 
+    private int courseId;        
+    
+    public Certificate() {
+    }
+
+    public Certificate(int certificateId, String certificateName, int courseId) {
+        this.certificateId = certificateId;
+        this.certificateName = certificateName;
+        this.courseId = courseId;
+    }
+
+    public int getCertificateId() {
+        return certificateId;
+    }
+
+    public void setCertificateId(int certificateId) {
+        this.certificateId = certificateId;
+    }
+
+    public String getCertificateName() {
+        return certificateName;
+    }
+
+    public void setCertificateName(String certificateName) {
+        this.certificateName = certificateName;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(int courseId) {
+        this.courseId = courseId;
+    }
+}
