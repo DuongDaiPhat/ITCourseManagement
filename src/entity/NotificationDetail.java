cda03a293ca3560efcd5568480ab6e7a7d016cb5
MinhBao0904 <dangminhbao0904@gmail.com>
2025-03-23 21:03:11 +0700

    MinhBao

---
+++ src/entity/NotificationDetail.java
@@ -0,0 +1,35 @@
+package entity;
+
+public class NotificationDetail {
+	private int notificationID;
+    private int userID;
+    private NotificationStatus status = NotificationStatus.UNREAD;
+    
+	public NotificationDetail(int notificationID, int userID, NotificationStatus status) {
+		super();
+		this.notificationID = notificationID;
+		this.userID = userID;
+		this.status = status;
+	}
+	
+	public int getNotificationID() {
+		return notificationID;
+	}
+	public void setNotificationID(int notificationID) {
+		this.notificationID = notificationID;
+	}
+	public int getUserID() {
+		return userID;
+	}
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+	public NotificationStatus getStatus() {
+		return status;
+	}
+	public void setStatus(NotificationStatus status) {
+		this.status = status;
+	}
+    
+    
+}
