c4f2a2191437d96388159242dbcc2db72125080b
DuongDaiPhat <duongphat521@gmail.com>
2025-03-24 14:16:10 +0700

    Create CourseDAO

--- src/backend/dao/CourseDAO.java
+++ src/backend/dao/CourseDAO.java
@@ -2,10 +2,14 @@ package backend.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import model.course.Courses;
+import model.course.Language;
+import model.course.Level;
+import model.course.ProgrammingLanguage;
 
 public class CourseDAO implements DAOInterface<Courses>{
 
@@ -15,27 +19,29 @@ public class CourseDAO implements DAOInterface<Courses>{
 	@Override
 	public int Insert(Courses t) throws SQLException {
 	    int result = 0;
-	    String sql = "INSERT INTO COURSES(COURSEID, COURSENAME, LANGUAGE, PROGRAMMINGLANGUAGE, LEVEL, USERID, THUMBNAILURL, PRICE, COURSEDESCRIPTION, CREATEDAT, UPDATEDAT) "
-	               + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+	    String sql = "INSERT INTO COURSES(COURSENAME, LANGUAGE, PROGRAMMINGLANGUAGE, LEVEL, USERID, THUMBNAILURL, PRICE, COURSEDESCRIPTION, CREATEDAT, UPDATEDAT) "
+	               + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 	    try (Connection con = DatabaseConnection.getConnection();
 	         PreparedStatement ps = con.prepareStatement(sql)) {
 
-	        ps.setString(1, t.getCourseID()+"");
-	        ps.setString(2, t.getCourseName());
-	        ps.setString(3, t.getLanguage()+"");
-	        ps.setString(4, t.getProgrammingLanguage()+"");
-	        ps.setString(5, t.getLevel()+"");
-	        ps.setString(6, t.getUserID()+"");
-	        ps.setString(7, t.getThumbnailURL());
-	        ps.setDouble(8, t.getPrice());
-	        ps.setString(9, t.getCourseDescription()+"");
-	        ps.setString(10, t.getCreatedAt()+"");
-	        ps.setString(11, t.getUpdatedAt()+"");
+	        ps.setString(1, t.getCourseName());
+	        ps.setString(2, t.getLanguage()+"");
+	        ps.setString(3, t.getProgrammingLanguage()+"");
+	        ps.setString(4, t.getLevel()+"");
+	        ps.setString(5, t.getUserID()+"");
+	        ps.setString(6, t.getThumbnailURL());
+	        ps.setDouble(7, t.getPrice());
+	        ps.setString(8, t.getCourseDescription()+"");
+	        ps.setString(9, t.getCreatedAt()+"");
+	        ps.setString(10, t.getUpdatedAt()+"");
 
 	        result = ps.executeUpdate();
 	        System.out.println("Insert executed. " + result + " row(s) affected");
 	    } catch (SQLException e) {
 	        e.printStackTrace();
+	    }
+	    finally {
+	    	DatabaseConnection.closeConnection();
 	    }
 	    return result;
 	}
@@ -58,6 +64,12 @@ public class CourseDAO implements DAOInterface<Courses>{
 			ps.setString(10, t.getLanguage() + "");
 			ps.setString(11, t.getLanguage() + "");
 		}
+	 	catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+		finally {
+	    	DatabaseConnection.closeConnection();
+	    }
 		return result;
 	}
 
@@ -74,24 +86,106 @@ public class CourseDAO implements DAOInterface<Courses>{
 	    } catch (SQLException e) {
 	        e.printStackTrace();
 	    }
+	    finally {
+	    	DatabaseConnection.closeConnection();
+	    }
 	    return result;
 	}
 	@Override
 	public ArrayList<Courses> SelectAll() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Courses> courseList = new ArrayList<Courses>();
+		String sql = "SELECT * FROM COURSES";
+		try(Connection con = DatabaseConnection.getConnection();
+			PreparedStatement ps = con.prepareStatement(sql);
+			ResultSet rs =ps.executeQuery();){
+			while(rs.next()) {
+				Courses course = new Courses();
+				course.setCourseID(rs.getInt("COURSEID"));
+				course.setCourseName(rs.getString("COURSENAME"));
+				course.setLanguage(Language.valueOf(rs.getString("LANGUAGE")));
+				course.setProgrammingLanguage(ProgrammingLanguage.valueOf(rs.getString("PROGRAMMINGLANGUAGE")));
+				course.setLevel(Level.valueOf(rs.getString("LEVEL")));
+				course.setUserID(rs.getInt("USERID"));
+				course.setThumbnailURL(rs.getString("THUMBNAILURL"));
+				course.setPrice(rs.getFloat("PRICE"));
+				course.setCourseDescription(rs.getString("COURSEDESCRIPTION"));
+				course.setCreatedAt(rs.getTimestamp("CREATEDAT").toLocalDateTime());
+				course.setUpdatedAt(rs.getTimestamp("UPDATEDAT").toLocalDateTime());
+				courseList.add(course);
+			}
+			System.out.println(courseList.size() + " row(s) found");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DatabaseConnection.closeConnection();
+		}
+		return courseList;
 	}
 
 	@Override
-	public Courses SelectByID(Courses t) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public Courses SelectByID(int id) throws SQLException {
+		Courses course = new Courses();
+		String sql = "SELECT * FROM COURSES WHERE COURSEID = " + String.valueOf(id);
+		try(Connection con = DatabaseConnection.getConnection();
+				PreparedStatement ps = con.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery()){
+			while(rs.next()) {
+				course.setCourseID(rs.getInt("COURSEID"));
+				course.setCourseName(rs.getString("COURSENAME"));
+				course.setLanguage(Language.valueOf(rs.getString("LANGUAGE")));
+				course.setProgrammingLanguage(ProgrammingLanguage.valueOf(rs.getString("PROGRAMMINGLANGUAGE")));
+				course.setLevel(Level.valueOf(rs.getString("LEVEL")));
+				course.setUserID(rs.getInt("USERID"));
+				course.setThumbnailURL(rs.getString("THUMBNAILURL"));
+				course.setPrice(rs.getFloat("PRICE"));
+				course.setCourseDescription(rs.getString("COURSEDESCRIPTION"));
+				course.setCreatedAt(rs.getTimestamp("CREATEDAT").toLocalDateTime());
+				course.setUpdatedAt(rs.getTimestamp("UPDATEDAT").toLocalDateTime());
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DatabaseConnection.closeConnection();
+		}
+		
+		return course;
 	}
 
 	@Override
 	public ArrayList<Courses> SelectByCondition(String condition) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<Courses> courseList = new ArrayList<Courses>();
+		String sql = "SELECT * FROM COURSES WHERE " + condition;
+		try(Connection con = DatabaseConnection.getConnection();
+				PreparedStatement ps = con.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery();){
+			while(rs.next()) {
+				Courses course = new Courses();
+				course.setCourseID(rs.getInt("COURSEID"));
+				course.setCourseName(rs.getString("COURSENAME"));
+				course.setLanguage(Language.valueOf(rs.getString("LANGUAGE")));
+				course.setProgrammingLanguage(ProgrammingLanguage.valueOf(rs.getString("PROGRAMMINGLANGUAGE")));
+				course.setLevel(Level.valueOf(rs.getString("LEVEL")));
+				course.setUserID(rs.getInt("USERID"));
+				course.setThumbnailURL(rs.getString("THUMBNAILURL"));
+				course.setPrice(rs.getFloat("PRICE"));
+				course.setCourseDescription(rs.getString("COURSEDESCRIPTION"));
+				course.setCreatedAt(rs.getTimestamp("CREATEDAT").toLocalDateTime());
+				course.setUpdatedAt(rs.getTimestamp("UPDATEDAT").toLocalDateTime());
+				courseList.add(course);
+			}
+			System.out.println(courseList.size() + " row(s) found");
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DatabaseConnection.closeConnection();
+		}
+		return courseList;
 	}
 	
 }
