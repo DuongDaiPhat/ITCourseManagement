9ddb7d116df701cd96fc298f687d70255c93bb75
KimDungwdf <6451071009@st.utc2.edu.vn>
2025-03-26 16:56:11 +0700

    Update_enum
    Điều chỉnh lại các enum

--- src/entity/CourseStatus.java
+++ src/entity/CourseStatus.java
@@ -1,5 +1,25 @@
 package entity;
 
 public enum CourseStatus {
-	IN_PROGRESS, FINISHED
+    IN_PROGRESS("in-progress"),
+    FINISHED("finished");
+
+    private final String value;
+
+    CourseStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public static CourseStatus fromString(String value) {
+        for (CourseStatus status : CourseStatus.values()) {
+            if (status.value.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        return null;
+    }
 }
