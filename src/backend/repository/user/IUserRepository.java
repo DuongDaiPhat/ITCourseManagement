b4f377f3a454aab1506e6c64b4b5fa6ad80243e6
DuongDaiPhat <duongphat521@gmail.com>
2025-04-12 01:57:31 +0700

    Create GetUserByUsername

--- src/backend/repository/user/IUserRepository.java
+++ src/backend/repository/user/IUserRepository.java
@@ -2,8 +2,12 @@ package backend.repository.user;
 
 import java.sql.SQLException;
 
+import model.user.Users;
+
 public interface IUserRepository {
 	public String GetUserPasswordByName(String username)throws SQLException;
 	
 	public boolean isExistUser(String username) throws SQLException;
+	
+	public Users GetUserByUsername(String username) throws SQLException;
 }
