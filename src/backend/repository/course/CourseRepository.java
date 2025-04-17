6cb9499642bde9cf0fc6f0c9a0fe1de660cbc8b7
KimDungwdf <6451071009@st.utc2.edu.vn>
2025-04-17 15:30:25 +0700

    Update CourseRepository.java

--- src/backend/repository/course/CourseRepository.java
+++ src/backend/repository/course/CourseRepository.java
@@ -14,95 +14,109 @@ import model.course.Language;
 import model.course.Level;
 import model.course.Technology;
 
-public class CourseRepository implements RepositoryInterface<Courses>, ICourseRepository{
+public class CourseRepository implements RepositoryInterface<Courses>, ICourseRepository {
 
 	public static CourseRepository getInstance() {
 		return new CourseRepository();
 	}
+
 	@Override
 	public int Insert(Courses t) throws SQLException {
-	    int result = 0;
-	    String sql = "INSERT INTO COURSES(COURSENAME, LANGUAGE, TECHNOLOGY, LEVEL,CATEGORY, USERID, THUMBNAILURL, PRICE, COURSEDESCRIPTION, CREATEDAT, UPDATEDAT) "
-	               + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-	    try (Connection con = DatabaseConnection.getConnection();
-	         PreparedStatement ps = con.prepareStatement(sql)) {
-
-	        ps.setString(1, t.getCourseName());
-	        ps.setString(2, t.getLanguage()+"");
-	        ps.setString(3, t.getTechnology()+"");
-	        ps.setString(4, t.getLevel()+"");
-	        ps.setString(5, t.getCategory() + "");
-	        ps.setString(6, t.getUserID()+"");
-	        ps.setString(7, t.getThumbnailURL());
-	        ps.setDouble(8, t.getPrice());
-	        ps.setString(9, t.getCourseDescription()+"");
-	        ps.setString(10, t.getCreatedAt()+"");
-	        ps.setString(11, t.getUpdatedAt()+"");
-
-	        result = ps.executeUpdate();
-	        System.out.println("Insert executed. " + result + " row(s) affected");
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    finally {
-	    	DatabaseConnection.closeConnection();
-	    }
-	    return result;
-	}
-
-	@Override
-	public int Update(Courses t) throws SQLException {
 		int result = 0;
-		String sql = "UPDATE COURSES SET COURSENAME = ?, LANGUAGE = ?, TECHNOLOGY = ?, LEVEL = ?, CATEGORY = ?,USERID = ?, THUMBNAILURL = ?, PRICE = ?, COURSEDESCRIPTION = ?, CREATEDAT = ?, UPDATEDAT = ? WHERE COURSESID = ?;";
-		try(Connection con = DatabaseConnection.getConnection();
-			PreparedStatement ps = con.prepareStatement(sql)){
+		String sql = "INSERT INTO COURSES(COURSENAME, LANGUAGE, TECHNOLOGY, LEVEL,CATEGORY, USERID, THUMBNAILURL, PRICE, COURSEDESCRIPTION, CREATEDAT, UPDATEDAT) "
+				+ "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		try (Connection con = DatabaseConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+
 			ps.setString(1, t.getCourseName());
 			ps.setString(2, t.getLanguage() + "");
 			ps.setString(3, t.getTechnology() + "");
 			ps.setString(4, t.getLevel() + "");
 			ps.setString(5, t.getCategory() + "");
 			ps.setString(6, t.getUserID() + "");
-			ps.setString(7, t.getThumbnailURL() + "");
-			ps.setString(8, t.getPrice() + "");
+			ps.setString(7, t.getThumbnailURL());
+			ps.setDouble(8, t.getPrice());
 			ps.setString(9, t.getCourseDescription() + "");
-			ps.setString(10, t.getUpdatedAt() + "");
-			ps.setString(11, t.getCourseID() + "");
+			ps.setString(10, t.getCreatedAt() + "");
+			ps.setString(11, t.getUpdatedAt() + "");
+
+			result = ps.executeUpdate();
+			System.out.println("Insert executed. " + result + " row(s) affected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DatabaseConnection.closeConnection();
 		}
-	 	catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-		finally {
-	    	DatabaseConnection.closeConnection();
-	    }
+		return result;
+	}
+
+	@Override
+	public int Update(Courses t) throws SQLException {
+		int result = 0;
+		String sql = "UPDATE COURSES SET COURSENAME = ?, LANGUAGE = ?, TECHNOLOGY = ?, LEVEL = ?, CATEGORY = ?, USERID = ?, THUMBNAILURL = ?, PRICE = ?, COURSEDESCRIPTION = ?, UPDATEDAT = ? WHERE COURSEID = ?";
+		try (Connection con = DatabaseConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+			ps.setString(1, t.getCourseName());
+			ps.setString(2, t.getLanguage().toString());
+			ps.setString(3, t.getTechnology().toString());
+			ps.setString(4, t.getLevel().toString());
+			ps.setString(5, t.getCategory().toString());
+			ps.setInt(6, t.getUserID());
+			ps.setString(7, t.getThumbnailURL());
+			ps.setFloat(8, t.getPrice());
+			ps.setString(9, t.getCourseDescription());
+			ps.setString(10, t.getUpdatedAt().toString());
+			ps.setInt(11, t.getCourseID());
+
+			result = ps.executeUpdate();
+			System.out.println("Update executed. " + result + " row(s) affected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DatabaseConnection.closeConnection();
+		}
 		return result;
 	}
 
 	@Override
 	public int Delete(Courses t) throws SQLException {
-	    int result = 0;
-	    String sql = "DELETE FROM COURSES WHERE COURSEID = ?";
-	    try (Connection con = DatabaseConnection.getConnection();
-	         PreparedStatement ps = con.prepareStatement(sql)) {
+		int result = 0;
+		String sql = "DELETE FROM COURSES WHERE COURSEID = ?";
+		try (Connection con = DatabaseConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
 
-	        ps.setString(1, t.getCourseID() + "");
-	        result = ps.executeUpdate();
-	        System.out.println("Delete executed. " + result + " row(s) affected");
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    finally {
-	    	DatabaseConnection.closeConnection();
-	    }
-	    return result;
+			ps.setString(1, t.getCourseID() + "");
+			result = ps.executeUpdate();
+			System.out.println("Delete executed. " + result + " row(s) affected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DatabaseConnection.closeConnection();
+		}
+		return result;
 	}
+
+	public int DeleteById(int courseId) throws SQLException {
+		int result = 0;
+		String sql = "DELETE FROM COURSES WHERE COURSEID = ?";
+		try (Connection con = DatabaseConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+
+			ps.setInt(1, courseId);
+			result = ps.executeUpdate();
+			System.out.println("Delete executed. " + result + " row(s) affected");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			DatabaseConnection.closeConnection();
+		}
+		return result;
+	}
+
 	@Override
 	public ArrayList<Courses> SelectAll() throws SQLException {
 		ArrayList<Courses> courseList = new ArrayList<Courses>();
 		String sql = "SELECT * FROM COURSES";
-		try(Connection con = DatabaseConnection.getConnection();
-			PreparedStatement ps = con.prepareStatement(sql);
-			ResultSet rs =ps.executeQuery();){
-			while(rs.next()) {
+		try (Connection con = DatabaseConnection.getConnection();
+				PreparedStatement ps = con.prepareStatement(sql);
+				ResultSet rs = ps.executeQuery();) {
+			while (rs.next()) {
 				Courses course = new Courses();
 				course.setCourseID(rs.getInt("COURSEID"));
 				course.setCourseName(rs.getString("COURSENAME"));
@@ -119,11 +133,9 @@ public class CourseRepository implements RepositoryInterface<Courses>, ICourseRe
 				courseList.add(course);
 			}
 			System.out.println(courseList.size() + " row(s) found");
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			DatabaseConnection.closeConnection();
 		}
 		return courseList;
@@ -133,10 +145,10 @@ public class CourseRepository implements RepositoryInterface<Courses>, ICourseRe
 	public Courses SelectByID(int id) throws SQLException {
 		Courses course = new Courses();
 		String sql = "SELECT * FROM COURSES WHERE COURSEID = " + String.valueOf(id);
-		try(Connection con = DatabaseConnection.getConnection();
+		try (Connection con = DatabaseConnection.getConnection();
 				PreparedStatement ps = con.prepareStatement(sql);
-				ResultSet rs = ps.executeQuery()){
-			while(rs.next()) {
+				ResultSet rs = ps.executeQuery()) {
+			while (rs.next()) {
 				course.setCourseID(rs.getInt("COURSEID"));
 				course.setCourseName(rs.getString("COURSENAME"));
 				course.setLanguage(Language.valueOf(rs.getString("LANGUAGE")));
@@ -150,14 +162,12 @@ public class CourseRepository implements RepositoryInterface<Courses>, ICourseRe
 				course.setCreatedAt(rs.getTimestamp("CREATEDAT").toLocalDateTime());
 				course.setUpdatedAt(rs.getTimestamp("UPDATEDAT").toLocalDateTime());
 			}
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			DatabaseConnection.closeConnection();
 		}
-		
+
 		return course;
 	}
 
@@ -165,10 +175,10 @@ public class CourseRepository implements RepositoryInterface<Courses>, ICourseRe
 	public ArrayList<Courses> SelectByCondition(String condition) throws SQLException {
 		ArrayList<Courses> courseList = new ArrayList<Courses>();
 		String sql = "SELECT * FROM COURSES WHERE " + condition;
-		try(Connection con = DatabaseConnection.getConnection();
+		try (Connection con = DatabaseConnection.getConnection();
 				PreparedStatement ps = con.prepareStatement(sql);
-				ResultSet rs = ps.executeQuery();){
-			while(rs.next()) {
+				ResultSet rs = ps.executeQuery();) {
+			while (rs.next()) {
 				Courses course = new Courses();
 				course.setCourseID(rs.getInt("COURSEID"));
 				course.setCourseName(rs.getString("COURSENAME"));
@@ -185,24 +195,22 @@ public class CourseRepository implements RepositoryInterface<Courses>, ICourseRe
 				courseList.add(course);
 			}
 			System.out.println(courseList.size() + " row(s) found");
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			DatabaseConnection.closeConnection();
 		}
 		return courseList;
 	}
+
 	@Override
 	public ArrayList<Courses> GetCoursesByUserID(int id) throws SQLException {
 		ArrayList<Courses> courseList = new ArrayList<Courses>();
 		String sql = "SELECT * FROM COURSES WHERE USERID = ?";
-		try(Connection con = DatabaseConnection.getConnection();
-			PreparedStatement ps = con.prepareStatement(sql);){
+		try (Connection con = DatabaseConnection.getConnection(); PreparedStatement ps = con.prepareStatement(sql);) {
 			ps.setInt(1, id);
-			ResultSet rs =ps.executeQuery();
-			while(rs.next()) {
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
 				Courses course = new Courses();
 				course.setCourseID(rs.getInt("COURSEID"));
 				course.setCourseName(rs.getString("COURSENAME"));
@@ -219,14 +227,11 @@ public class CourseRepository implements RepositoryInterface<Courses>, ICourseRe
 				courseList.add(course);
 			}
 			System.out.println(courseList.size() + " row(s) found");
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			DatabaseConnection.closeConnection();
 		}
 		return courseList;
 	}
-	
 }
